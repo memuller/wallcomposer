d4c6cca14a907bb5391a83e656e4ee7287924cf7
Matheus E. Muller <hello@memuller.com>
2017-04-28 11:44:52 -0300

    Moves Types to a common JS module.

---
+++ src/Common.js
@@ -0,0 +1,20 @@
+//@flow
+
+export type BasicDisplay = {
+  width: Number, height: Number, x: Number, y: Number
+}
+
+export type ElectronDisplay = {
+  bounds :BasicDisplay
+}
+
+export type Bounds = {
+  x: number, y: number
+}
+
+export type DisplayProperties = {
+  width: number,
+  height: number,
+  x: number,
+  y: number
+}
